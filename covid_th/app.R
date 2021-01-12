39d81a4fc5971ddcc2a336655df0097518e86211
ben <benjaminsw@live.com>
2021-01-12 10:54:55 +0000

    added main plot

--- covid_th/app.R
+++ covid_th/app.R
@@ -7,41 +7,32 @@ data <-  fromJSON(rawToChar(res$content))
 df <- data$Data
 df$Date <- as.Date(format(as.Date(df$Date, "%m/%d/%Y"), format="%d %b %Y"),"%d %b %Y")
 
-Year <- c(2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010)
-Auckland <- c(1760, 1549, 1388, 1967, 1326, 1765, 1814, 1693, 1502, 1751)
-Wellington <- c(2176, 3154, 1138, 1196, 2132, 3176, 4181, 5169, 3150, 4175)
-Lyttelton <- c(2176, 3154, 1138, 1196, 2132, 3176, 4181, 5169, 3150, 4175)
-my_data <- as.data.frame(cbind(Year,Auckland,Wellington, Lyttelton))
+new_confirmed_df <- data.frame(time=df$Date,  value=df$NewConfirmed)
+new_confirmed_df <- xts(new_confirmed_df[,-1], order.by=new_confirmed_df[,1])
+index(new_confirmed_df) <- index(new_confirmed_df)-0
 
 ui <- fluidPage(
     titlePanel("Covid-19 Cases Thailand"),
     
-    sidebarLayout(
-        sidebarPanel(
-            
-            #sliderInput("date_range", 
-                        #"Choose Date Range:", 
-                        #min = as.Date("2016-02-01"), max = Sys.Date(), 
-                        #value = c(as.Date("2016-02-25"), Sys.Date())
-            #)
-            sliderInput("range", 
-                        label = "Choose a start and end year:",
-                        min = df$Date[1], 
-                        max = df$Date[nrow(df)], 
-                        value = c(df$Date[1], df$Date[nrow(df)]))
-        ),
+    #sidebarLayout(
+        #sidebarPanel(
+            #sliderInput("range", 
+                        #label = "Choose a start and end year:",
+                        #min = df$Date[1], 
+                        #max = df$Date[nrow(df)], 
+                        #value = c(df$Date[1], df$Date[nrow(df)]))
+        #),
         
         mainPanel(
-            tableOutput("DataTable")
+            dygraphOutput("new_confirmed_plot")
         )
-    )
+    #)
 )
 server <- function(input, output) {
     
-    output$DataTable <- renderTable({
-        dt <- my_data[my_data$Year >= input$range[1] & my_data$Year <= input$range[2],]
-        dt[,c("Year",input$var)]
-    },include.rownames=FALSE)
+    output$new_confirmed_plot <- renderDygraph({
+        dygraph(new_confirmed_df)
+    })
     
 }
 shinyApp(ui, server)
